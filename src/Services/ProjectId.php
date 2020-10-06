8a245d2dfdc00f1a4e3f9804e095bdf7dc2717db
reapex <tr7296228@gmail.com>
2020-10-06 16:52:30 +0200

    Initial commit

--- src/Services/ProjectId.php
+++ src/Services/ProjectId.php
@@ -6,7 +6,7 @@ final class ProjectId
 {
     public function getFormationProjectID()
     {
-        return [19105266, 19108592, 19676897, 17026977, 19872619, 21095567];
+        return [19105266, 19108592, 19676897, 17026977, 19872619];
 
     }
 

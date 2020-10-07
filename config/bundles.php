a00a4340ce7b5ea3c517e0fe00f3e3449b77f8d0
reapex <tr7296228@gmail.com>
2020-10-07 21:01:40 +0200

    Update

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,5 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    CMEN\GoogleChartsBundle\CMENGoogleChartsBundle::class => ['all' => true],
 ];

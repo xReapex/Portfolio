92e021e0b5791b539bd6eef827689a3d5cb6879d
reapex <tr7296228@gmail.com>
2020-10-14 15:25:16 +0200

    Optimisation

--- src/Controller/about/AboutController.php
+++ src/Controller/about/AboutController.php
@@ -3,14 +3,20 @@
 namespace App\Controller\about;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class AboutController
+ * @package App\Controller\about
+ */
 class AboutController extends AbstractController
 {
     /**
-     * @Route("/about", name="app.about")
+     *@Route("/about", name="app.about")
+     * @return Response
      */
-    public function showAbout()
+    public function showAbout(): Response
     {
         return $this->render('about/about.html.twig');
     }

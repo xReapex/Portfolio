5078462a0cf3ad7a6ec406e7b5ca5b9913dd80c2
Reapex <tr7296228@gmail.com>
2020-10-15 13:50:59 +0200

    Stable authenticator

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,15 +2,26 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
@@ -33,4 +44,47 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    /**
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $encoder
+     * @return RedirectResponse|Response
+     * @Route("/register", name="app.register")
+     */
+    public function createUser(Request $request, UserPasswordEncoderInterface $encoder)
+    {
+
+        $user = new User();
+        $form = $this->createForm(FormType::class, $user);
+
+        $form
+            ->add('Username', TextType::class)
+            ->add('Email', EmailType::class)
+            ->add('Password', PasswordType::class)
+            ->add('Submit', SubmitType::class);
+
+        if ($request->isMethod('POST')) {
+
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+                $hash = $encoder->encodePassword($user, $user->getPassword());
+
+                $user->setPassword($hash);
+                $user->setRoles(["ROLE_USER"]);
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
+
+                return $this->redirectToRoute('app_login');
+            }
+        }
+
+        return $this->render('security/register.html.twig', array(
+            'form' => $form->createView()
+        ));
+
+    }
+
 }

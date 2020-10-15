0e8d16a0777c17e501d7779e19bd6d00ed6d3294
reapex <tr7296228@gmail.com>
2020-10-15 10:06:37 +0200

    Add timeline in about

--- src/Controller/about/AboutController.php
+++ src/Controller/about/AboutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\about;
 
+use App\Services\ProjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,11 +14,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class AboutController extends AbstractController
 {
     /**
-     *@Route("/about", name="app.about")
-     *@return Response
+     * @Route("/about", name="app.about")
+     * @param ProjectManager $manager
+     * @return Response
      */
-    public function showAbout(): Response
+    public function showAbout(ProjectManager $manager): Response
     {
-        return $this->render('about/about.html.twig');
+        $timeline = $manager->getParcoursTimeline();
+        return $this->render('about/about.html.twig',
+        [
+            "timeline" => $timeline
+        ]);
     }
 }

d6080b89327105c6259bbb5d1c96e2dbc601c8e4
Reapex <tr7296228@gmail.com>
2020-10-19 09:46:08 +0200

    sendEmail, sendAllEmails, sendRegistrationEmail

--- src/Services/EmailManager.php
+++ src/Services/EmailManager.php
@@ -3,19 +3,22 @@
 namespace App\Services;
 
 use App\Repository\NewsletterRepository;
+use Twig\Environment;
 
 class EmailManager
 {
 
-    protected $tokenGenerator;
-    protected $newsletterRepository;
-    protected $mailer;
+    private $tokenGenerator;
+    private $newsletterRepository;
+    private $mailer;
+    private $twig;
 
-    public function __construct(TokenGenerator $tokenGenerator, NewsletterRepository $newsletterRepository, \Swift_Mailer $mailer)
+    public function __construct(TokenGenerator $tokenGenerator, NewsletterRepository $newsletterRepository, \Swift_Mailer $mailer, Environment $twig)
     {
         $this->tokenGenerator = $tokenGenerator;
         $this->newsletterRepository = $newsletterRepository;
         $this->mailer = $mailer;
+        $this->twig = $twig;
     }
 
     public function sendAllEmails()
@@ -30,12 +33,21 @@ class EmailManager
 
     public function sendEmail($email)
     {
-
         $message = (new \Swift_Message('Hello Email'))
             ->setFrom('reapexautomaticemail@gmail.com')
             ->setTo($email)
+            ->setBody("toast")
+        ;
+        $this->mailer->send($message);
+    }
+
+    public function sendRegistrationEmail($email, $token)
+    {
+        $message = (new \Swift_Message('Email de confirmation !'))
+            ->setFrom('reapexautomaticemail@gmail.com')
+            ->setTo($email)
             ->setBody(
-                $this->renderView(
+                $this->twig->render(
                 // templates/emails/registration.html.twig
                     'emails/registration.html.twig',
                     ['token' => $token]

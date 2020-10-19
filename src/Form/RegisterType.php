57800e31eb3d28a07b07845cabe52d172501924a
Reapex <tr7296228@gmail.com>
2020-10-19 16:12:00 +0200

    Check 2 times password

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -19,8 +20,13 @@ class RegisterType extends AbstractType
                 'label' => "Pseudo"
             ])
             ->add('Email', EmailType::class)
-            ->add('Password', PasswordType::class, [
-                'label' => "Mot de passe"
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe doivent être identiques.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répéter le mot de passe'],
             ])
             ->add('Envoyer', SubmitType::class);
     }

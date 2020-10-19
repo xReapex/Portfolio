43614c6cf959392a231176b61f8fbc7751f01ef8
Reapex <tr7296228@gmail.com>
2020-10-19 10:53:07 +0200

    Add findToken method

--- src/Services/TokenManager.php
+++ src/Services/TokenManager.php
@@ -2,12 +2,29 @@
 
 namespace App\Services;
 
+use App\Repository\UserRepository;
+
 class TokenManager
 {
+
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function createToken($length)
     {
         $token = openssl_random_pseudo_bytes($length);
         $token = bin2hex($token);
         return $token;
+    }
+
+    public function findToken($token)
+    {
+        return $this->userRepository->findOneBy([
+            'token' => $token
+        ]);
     }
 }

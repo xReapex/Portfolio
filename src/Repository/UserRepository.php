0f60173f2658b7e87c5dc0345b4f4dbad7ae8de3
Reapex <tr7296228@gmail.com>
2020-11-07 17:24:03 +0100

    Pagination

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\Query;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
@@ -64,4 +65,15 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         ;
     }
     */
+    /**
+     * @return Query
+     */
+    public function findLastest(): Query
+    {
+        return $this->createQueryBuilder('p')
+            ->setMaxResults(3)
+            ->addOrderBy('p.id', 'ASC')
+            ->getQuery();
+    }
+
 }
